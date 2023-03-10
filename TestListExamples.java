55f6b30f3714ab6e72a873d82c67322e57125a23
gewnwong <gewong@gmail.com>
2023-03-10 14:36:16 -0800

    Wrote grade.sh for lab report 5

--- TestListExamples.java
+++ TestListExamples.java
@@ -3,13 +3,14 @@ import org.junit.*;
 import java.util.Arrays;
 import java.util.List;
 
-class IsMoon implements StringChecker {
+class IsA implements StringChecker {
   public boolean checkString(String s) {
-    return s.equalsIgnoreCase("moon");
+    return s.equalsIgnoreCase("a");
   }
 }
 
 public class TestListExamples {
+
   @Test(timeout = 500)
   public void testMergeRightEnd() {
     List<String> left = Arrays.asList("a", "b", "c");
@@ -17,5 +18,16 @@ public class TestListExamples {
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter() {
+    List<String> s1 = Arrays.asList("a", "b", "c");
+    List<String> s2 = Arrays.asList("d", "a");
+    List<String> expect = Arrays.asList("a");
+    List<String> result1 = ListExamples.filter(s1, new IsA());
+    List<String> result2 = ListExamples.filter(s2, new IsA());
+    assertEquals(expect, result1);
+    assertEquals(expect, result2);
   }
 }
--- grade.sh
+++ grade.sh
@@ -1,5 +1,70 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 git clone $1 student-submission
-echo 'Finished cloning'
+echo "Finished cloning"
+
+if [[ -f student-submission/ListExamples.java ]]
+then
+    echo "ListExamples.java found"
+else
+    echo "ListExamples.java not found"
+    echo "Score: Invalid"
+    exit 1
+fi
+
+cp student-submission/ListExamples.java ./
+
+javac -cp $CPATH *.java &> compile.txt
+
+# Check for if student-submission compiles
+FAIL=`grep -c error: compile.txt`
+if [[ $FAIL -ne 0 ]] 
+then
+    echo "Compile failed with output:"
+    cat compile.txt
+    echo "Compile of the student submission failed"
+    echo "Score: Invalid"
+    exit 1
+else
+    echo "Compile of the student submission succeeded"
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+# Check for FAILURES!!!
+FAILURES=`grep -c FAILURES!!! junit-output.txt`
+
+if [[ $FAILURES -eq 0 ]]
+then 
+    echo 'All tests passed'
+    # Check for number of tests 
+    RESULT=`grep "OK" junit-output.txt`
+    TOTAL=${RESULT:4:2}
+    SECOND_DIGIT=${TOTAL:1:1}
+    if [[ $SECOND_DIGIT = ")" ]]
+    then
+        TOTAL=${RESULT:4:1}
+    fi
+    echo "Score: $TOTAL/$TOTAL"
+else
+    RESULT=`grep "Tests run:" junit-output.txt`
+
+    TOTAL=${RESULT:11:2}
+    SECOND_DIGIT=${TOTAL:1:1}
+    if [[ $SECOND_DIGIT = "," ]]
+    then
+        TOTAL=${RESULT:11:1}
+    fi
+
+    FAILED=${RESULT:25:2}
+    SECOND_DIGIT=${FAILED:1:1}
+    if [[ $SECOND_DIGIT = " " ]]
+    then 
+        FAILED=${RESULT:25:1}
+    fi
+
+    echo "JUnit output was:"
+    cat junit-output.txt
+    echo "Score: $(($TOTAL - $FAILED))/$TOTAL"
+fi
